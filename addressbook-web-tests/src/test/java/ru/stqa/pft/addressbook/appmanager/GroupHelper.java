38017babe05eaeec897992dfd94f5bdc738dc408
vladyem <69014826+vladyem@users.noreply.github.com>
2020-10-06 21:21:07 -0700

    Revert "l2m12 finished"
    This reverts commit 14b0fca041a13edfc509ef148b01ff90d084faee.

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/GroupHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/GroupHelper.java
@@ -35,12 +35,4 @@ public class GroupHelper extends Helperbase {
   public void selectGroup() {
     click(By.name("selected[]"));
   }
-
-  public void initGroupModification() {
-    click(By.name("edit"));
-  }
-
-  public void submitGroupModification() {
-    click(By.name("update"));
-  }
 }
