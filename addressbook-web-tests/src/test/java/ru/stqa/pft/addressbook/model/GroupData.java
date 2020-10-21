70bc3e8e8057be850986ba8b875f4ea44f8a22f6
vladyem <69014826+vladyem@users.noreply.github.com>
2020-10-21 14:36:54 -0700

    Revert "l4m8"
    This reverts commit efe08e748121a43262f1e389da9e0a013986ddb3.

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/GroupData.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/GroupData.java
@@ -3,30 +3,26 @@ package ru.stqa.pft.addressbook.model;
 import java.util.Objects;
 
 public class GroupData {
-  private int id;
+  private final String id;
   private final String name;
   private final String header;
   private final String footer;
 
   public GroupData(String name, String header, String footer) {
-    this.id = 0;
+    this.id = null;
     this.name = name;
     this.header = header;
     this.footer = footer;
   }
 
-  public GroupData(int id, String name, String header, String footer) {
+  public GroupData(String id, String name, String header, String footer) {
     this.id = id;
     this.name = name;
     this.header = header;
     this.footer = footer;
   }
 
-  public int getId() { return id; }
-
-  public void setId(int id) {
-    this.id = id;
-  }
+  public String getId() { return id; }
 
   public String getName() {
     return name;
@@ -45,7 +41,7 @@ public class GroupData {
     if (this == o) return true;
     if (o == null || getClass() != o.getClass()) return false;
     GroupData groupData = (GroupData) o;
-    return id == groupData.id &&
+    return Objects.equals(id, groupData.id) &&
             Objects.equals(name, groupData.name);
   }
 
